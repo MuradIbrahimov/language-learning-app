22ef317ebaa87400ca5396d92945a3502b901a70
MuradIbrahimov <muradibrahimm@yahoo.com>
2023-05-11 01:53:39 +0400

    More user friendly interface added

--- Language-learning-app/src/main/kotlin/Main.kt
+++ Language-learning-app/src/main/kotlin/Main.kt
@@ -1,4 +1,3 @@
-// Word class
 abstract class Word(private val word: String, private val translation: String) {
     fun getWord(): String {
         return word
@@ -8,7 +7,6 @@ abstract class Word(private val word: String, private val translation: String) {
     }
 }
 
-// VocabularyWord class
 class VocabularyWord(word: String, translation: String, private val picture: String, private val category: String) : Word(word, translation) {
     fun getPicture(): String {
         return picture
@@ -18,30 +16,46 @@ class VocabularyWord(word: String, translation: String, private val picture: Str
     }
 }
 
-// Phrase class
 class Phrase(word: String, translation: String, private val audio: String) : Word(word, translation) {
     fun getAudio(): String {
         return audio
     }
 }
 
-// Language interface
 interface Language {
     fun getWord(): Word
     fun getPhrase(): Phrase
-}
+    class LanguageImpl(private val vocabulary: List<VocabularyWord>, private val phrases: List<Phrase>) : Language {
+        private var currentIndex = -1
 
-// LanguageImpl class
-class LanguageImpl(private val vocabulary: List<VocabularyWord>, private val phrases: List<Phrase>) : Language {
-    override fun getWord(): Word {
-        return vocabulary.random()
-    }
-    override fun getPhrase(): Phrase {
-        return phrases.random()
-    }
-}
+        override fun getWord(): Word {
+            currentIndex++
+            if (currentIndex >= vocabulary.size) {
+                currentIndex = 0
+            }
+            val word = vocabulary[currentIndex]
+           // println("Selected word: ${word.getWord()}")
+            return word
+        }
 
-// App class
+        override fun getPhrase(): Phrase {
+            val phrase = phrases.random()
+            println("Selected phrase: ${phrase.getWord()}")
+            return phrase
+        }
+
+        fun getProperty(property: Int): String? {
+            val word = getWord() as? VocabularyWord ?: return null
+            return when (property) {
+                1 -> word.getTranslation()
+                2 -> word.getPicture()
+                3 -> word.getCategory()
+                else -> null
+            }
+        }
+    }
+
+}
 class LanguageLearningApp(private val language: Language) {
     private val vocabularyList = mutableListOf<VocabularyWord>()
     private val phraseList = mutableListOf<Phrase>()
@@ -62,39 +76,84 @@ class LanguageLearningApp(private val language: Language) {
         return language.getPhrase()
     }
 }
-
 fun main() {
-    val englishVocabulary = listOf(
-        VocabularyWord("cat", "gato", "cat.jpg", "animals"),
-        VocabularyWord("dog", "perro", "dog.jpg", "animals"),
-        VocabularyWord("apple", "manzana", "apple.jpg", "food"),
-        VocabularyWord("banana", "plátano", "banana.jpg", "food")
+    //language object
+    val AzerbaijanVocabulary = listOf(
+        VocabularyWord("pişik", "cat", "cat.jpg", "animals"),
+        VocabularyWord("it", "dog", "dog.jpg", "animals"),
+        VocabularyWord("alma", "apple", "apple.jpg", "fruits/food"),
+        VocabularyWord("banan", "banana", "banana.jpg", "fruits/food"),
+        VocabularyWord("dəmir", "iron", "iron.jpg", "materials"),
+        VocabularyWord("altın", "gold", "gold.jpg", "materials")
     )
-    val spanishPhrases = listOf(
-        Phrase("¿Cómo te llamas?", "What is your name?", "name.wav"),
-        Phrase("Me gusta la pizza.", "I like pizza.", "pizza.wav")
+    val AzerbaijanPhrases = listOf(
+        Phrase("Adın nədir?", "What is your name?", "name.wav"),
+        Phrase("Mənim 5 yaşım var", "I am 5 years old", "age.wav")
     )
-    val spanishLanguage = LanguageImpl(englishVocabulary, spanishPhrases)
+    val EnglishLanguage = Language.LanguageImpl(AzerbaijanVocabulary, AzerbaijanPhrases)
 
-    val app = LanguageLearningApp(spanishLanguage)
+    //app object
+    val app = LanguageLearningApp(EnglishLanguage)
 
     app.addVocabularyWord(VocabularyWord("orange", "naranja", "orange.jpg", "food"))
     app.addPhrase(Phrase("¿Qué tal?", "How are you?", "howareyou.wav"))
 
-    val randomWord = app.getWord()
-    val randomPhrase = app.getPhrase()
+    var word = app.getWord()
+    var exit = false
+    while (!exit) {
+        printMenu()
+        print("Enter your choice: ")
+        when (readLine()?.toIntOrNull() ?: -1) {
+            0 -> {
+                exit = true
+                println("Goodbye!")
+            }
+            1 -> {
+                word = app.getWord()
+                println("Selected word: ${word.getWord()}")
+            }
+            2 -> {
+                val phrase = app.getPhrase()
+                //println("Phrase: ${phrase.getWord()}")
+                println("Translation: ${phrase.getTranslation()}")
+                println("Audio: ${phrase.getAudio()}")
+            }
+            3 -> {
+                println("Translation: ${word.getTranslation()}")
+            }
+            4 -> {
+                val word1 = word as? VocabularyWord
+                if (word1 != null) {
+                    println("Category: ${word1.getPicture()}")
+                } else {
+                    println("Invalid option")
+                }
+            }
+            5 -> {
+                val word1 = word as? VocabularyWord
+                if (word1 != null) {
+                    println("Category: ${word1.getCategory()}")
+                } else {
+                    println("Invalid option")
+                }
+            }
+            6 -> {
+                word = app.getWord()
+                println("Selected word: ${word.getWord()}")
 
-    if (randomWord is VocabularyWord) {
-        println("Vocabulary Word:")
-        println("Word: ${randomWord.getWord()}")
-        println("Translation: ${randomWord.getTranslation()}")
-        println("Picture: ${randomWord.getPicture()}")
-        println("Category: ${randomWord.getCategory()}")
+            }
+            else -> println("Invalid option")
+        }
     }
-    if (randomPhrase is Phrase) {
-        println("Phrase:")
-        println("Word: ${randomPhrase.getWord()}")
-        println("Translation: ${randomPhrase.getTranslation()}")
-        println("Audio: ${randomPhrase.getAudio()}")
-    }
+}
+
+private fun printMenu() {
+    println("Available options\n press")
+    println("0 - to quit\n" +
+            "1 - random word\n" +
+            "2 - random phrase\n" +
+            "3 - translation\n" +
+            "4 - picture\n" +
+            "5 - category\n" +
+            "6 - next word\n")
 }
